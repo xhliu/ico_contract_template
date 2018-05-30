a8ed5c6e02969133142a4994542e0696f47552cd
Xiaohui Liu <whulxh@gmail.com>
2018-05-30 11:18:01 -0700

    Update using BOX token contract

--- ico.sol
+++ ico.sol
@@ -1,252 +1,254 @@
-pragma solidity ^0.4.11;
+pragma solidity 0.4.18;
 
-/**
- * ERC 20 token
- *
- * https://github.com/ethereum/EIPs/issues/20
- */
-contract MoacToken  {
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a / b;
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+contract Ownable {
+    address public owner;
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner public {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+contract ERC20 {
+    uint256 public totalSupply;
+    function balanceOf(address who) public constant returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function allowance(address owner, address spender) public constant returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract StandardToken is ERC20 {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value > 0);
+
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_from != address(0));
+        require(_to != address(0));
+
+        uint256 _allowance = allowed[_from][msg.sender];
+
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
     }
+}
 
-    mapping(address => uint256) balances;
-    mapping(address => uint256) redeem;
-    mapping (address => mapping (address => uint256)) allowed;
+contract BOXToken is StandardToken, Ownable {
 
-    uint256 public totalSupply;
-    string public name = "MoacToken Token";
-    string public symbol = "MOAC";
+    string public name = "BOX Token";
+    string public symbol = "BOX";
     uint public decimals = 18;
 
-    uint public startBlock; //crowdsale start block (set in constructor)
-    uint public endBlock; //crowdsale end block (set in constructor)
+    // The token allocation
+    uint public constant TOTAL_SUPPLY       = 3000000000e18;
+    uint public constant ALLOC_ECOSYSTEM    =  900000000e18; // 30%
+    uint public constant ALLOC_FOUNDATION   =  600000000e18; // 20%
+    uint public constant ALLOC_TEAM         =  450000000e18; // 15%
+    uint public constant ALLOC_PARTNER      =  300000000e18; // 10%
+    uint public constant ALLOC_SALE         =  750000000e18; // 25%
 
-    address public founder = 0x0;
-    address public owner = 0x0;
+    // wallets
+    address public constant WALLET_ECOSYSTEM    = 0x49dE776A181603b11116E7DaB15d84BE6711D54A;
+    address public constant WALLET_FOUNDATION   = 0x8546a5a4b3BBE86Bf57fC9F5E497c770ae5D0233;
+    address public constant WALLET_TEAM         = 0x9f255092008F6163395aEB35c4Dec58a1ecbdFd6;
+    address public constant WALLET_PARTNER      = 0xD6d64A62A7fF8F55841b0DD2c02d5052457bCA6c;
+    address public constant WALLET_SALE         = 0x55aaeC60E116086AC3a5e4fDC74b21de9B91CC53;
 
-    // signer address 
-    address public signer = 0x0;
+    // 2 groups of lockup
+    mapping(address => uint256) public contributors_locked;
+    mapping(address => uint256) public investors_locked;
 
-    // price is defined by levels
-    uint256 public levelOneTokenNum = 30000000 * 10**18; //first level 
-    uint256 public levelTwoTokenNum = 50000000 * 10**18; //second level 
-    uint256 public levelThreeTokenNum = 75000000 * 10**18; //third level 
-    uint256 public levelFourTokenNum = 100000000 * 10**18; //fourth level 
-    
-    //max amount raised during crowdsale
-    uint256 public etherCap = 1000000 * 10**18;  
-    uint public transferLockup = 370285; 
-    uint public founderLockup = 86400; 
-    
-    uint256 public founderAllocation = 100 * 10**16; 
-    bool public founderAllocated = false; 
+    // 2 types of releasing
+    mapping(address => uint256) public contributors_countdownDate;
+    mapping(address => uint256) public investors_deliveryDate;
 
-    uint256 public saleTokenSupply = 0; 
-    uint256 public saleEtherRaised = 0; 
-    bool public halted = false; 
+    // MODIFIER
 
-    event Donate(uint256 eth, uint256 fbt);
-    event AllocateFounderTokens(address indexed sender);
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    event print(bytes32 msg);
+    // checks if the address can transfer certain amount of tokens
+    modifier canTransfer(address _sender, uint256 _value) {
+        require(_sender != address(0));
 
-    function MoacToken(address founderInput, address signerInput, uint startBlockInput, uint endBlockInput) {
-        founder = founderInput;
-        signer = signerInput;
-        startBlock = startBlockInput;
-        endBlock = endBlockInput;
-        owner = msg.sender;
-    }
+        uint256 remaining = balances[_sender].sub(_value);
+        uint256 totalLockAmt = 0;
 
-    //price based on current token supply
-    function price() constant returns(uint256) {
-        if (totalSupply<levelOneTokenNum) return 1600;
-        if (totalSupply>=levelOneTokenNum && totalSupply < levelTwoTokenNum) return 1000;
-        if (totalSupply>=levelTwoTokenNum && totalSupply < levelThreeTokenNum) return 800;
-        if (totalSupply>=levelThreeTokenNum && totalSupply < levelFourTokenNum) return 730;
-        if (totalSupply>=levelFourTokenNum) return 680;
-        return 1600;
-    }
-
-    // price() exposed for unit tests
-    function testPrice(uint256 currentSupply) constant returns(uint256) {
-        if (currentSupply<levelOneTokenNum) return 1600;
-        if (currentSupply>=levelOneTokenNum && currentSupply < levelTwoTokenNum) return 1000;
-        if (currentSupply>=levelTwoTokenNum && currentSupply < levelThreeTokenNum) return 800;
-        if (currentSupply>=levelThreeTokenNum && currentSupply < levelFourTokenNum) return 730;
-        if (currentSupply>=levelFourTokenNum) return 680;
-        return 1600;
-    }
-
-
-    // Donate entry point
-    function donate( bytes32 hash) payable {
-        print(hash);
-        if (block.number<startBlock || block.number>endBlock || (saleEtherRaised + msg.value)>etherCap || halted) throw;
-        uint256 tokens = (msg.value * price());
-        balances[msg.sender] = (balances[msg.sender] + tokens);
-        totalSupply = (totalSupply + tokens);
-        saleEtherRaised = (saleEtherRaised + msg.value);
-        //immediately send Ether to founder address
-        if (!founder.call.value(msg.value)()) throw; 
-        Donate(msg.value, tokens);
-    }
-
-    /**
-     * Set up founder address token balance.
-     */
-    function allocateFounderTokens() {
-        if (msg.sender!=founder) throw;
-        if (block.number <= endBlock + founderLockup) throw;
-        if (founderAllocated) throw;
-        balances[founder] = (balances[founder] + saleTokenSupply * founderAllocation / (1 ether));
-        totalSupply = (totalSupply + saleTokenSupply * founderAllocation / (1 ether));
-        founderAllocated = true;
-        AllocateFounderTokens(msg.sender);
-    }
-
-    /**
-     * For offline donation, executed by signer only. only available during the sale
-     */
-    function offlineDonate(uint256 offlineTokenNum, uint256 offlineEther) {
-        if (msg.sender!=signer) throw;
-        if (block.number >= endBlock) throw; //offline can be done only before end block
-        
-        //check if overflow
-        if( (totalSupply +offlineTokenNum) > totalSupply && (saleEtherRaised + offlineEther)>saleEtherRaised){
-            totalSupply = (totalSupply + offlineTokenNum);
-            balances[founder] = (balances[founder] + offlineTokenNum );
-            saleEtherRaised = (saleEtherRaised + offlineEther);
+        if (contributors_locked[_sender] > 0) {
+            totalLockAmt = totalLockAmt.add(getLockedAmount_contributors(_sender));
         }
-    }
 
-
-    /** 
-     * emergency adjust if incorrectly set by signer, only available during the sale
-     */
-    function offlineAdjust(uint256 offlineTokenNum, uint256 offlineEther) {
-        if (msg.sender!=founder) throw;
-        if (block.number >= endBlock) throw; //offline can be done only before end block
-        
-        //check if overflow
-        if( (totalSupply - offlineTokenNum) > 0 && (saleEtherRaised - offlineEther) > 0 && (balances[founder] - offlineTokenNum)>0){
-            totalSupply = (totalSupply - offlineTokenNum);
-            balances[founder] = (balances[founder] - offlineTokenNum );
-            saleEtherRaised = (saleEtherRaised - offlineEther);
+        if (investors_locked[_sender] > 0) {
+            totalLockAmt = totalLockAmt.add(getLockedAmount_investors(_sender));
         }
+
+        require(remaining >= totalLockAmt);
+
+        _;
     }
 
+    // EVENTS
+    event UpdatedLockingState(string whom, address indexed to, uint256 value, uint256 date);
 
-    //check for redeemed balance
-    function redeemBalanceOf(address _owner) constant returns (uint256 balance) {
-        return redeem[_owner];
+    // FUNCTIONS
+
+    function BOXToken() public {
+        balances[msg.sender] = TOTAL_SUPPLY;
+        totalSupply = TOTAL_SUPPLY;
+
+        // do the distribution of the token, in token transfer
+        transfer(WALLET_ECOSYSTEM, ALLOC_ECOSYSTEM);
+        transfer(WALLET_FOUNDATION, ALLOC_FOUNDATION);
+        transfer(WALLET_TEAM, ALLOC_TEAM);
+        transfer(WALLET_PARTNER, ALLOC_PARTNER);
+        transfer(WALLET_SALE, ALLOC_SALE);
     }
 
-    /**
-     * redeem token in MOAC network
-     */
-    function redeemToken(uint256 tokenNum) {
-        if (block.number <= (endBlock + transferLockup) && msg.sender!=founder) throw; 
-        if( balances[msg.sender] < tokenNum ) throw;
-        balances[msg.sender] = (balances[msg.sender] - tokenNum );
-        redeem[msg.sender] += tokenNum;
+    // get contributors' locked amount of token
+    // this lockup will be released in 8 batches which take place every 180 days
+    function getLockedAmount_contributors(address _contributor)
+        public
+		constant
+		returns (uint256)
+	{
+        uint256 countdownDate = contributors_countdownDate[_contributor];
+        uint256 lockedAmt = contributors_locked[_contributor];
+
+        if (now <= countdownDate + (180 * 1 days)) {return lockedAmt;}
+        if (now <= countdownDate + (180 * 2 days)) {return lockedAmt.mul(7).div(8);}
+        if (now <= countdownDate + (180 * 3 days)) {return lockedAmt.mul(6).div(8);}
+        if (now <= countdownDate + (180 * 4 days)) {return lockedAmt.mul(5).div(8);}
+        if (now <= countdownDate + (180 * 5 days)) {return lockedAmt.mul(4).div(8);}
+        if (now <= countdownDate + (180 * 6 days)) {return lockedAmt.mul(3).div(8);}
+        if (now <= countdownDate + (180 * 7 days)) {return lockedAmt.mul(2).div(8);}
+        if (now <= countdownDate + (180 * 8 days)) {return lockedAmt.mul(1).div(8);}
+
+        return 0;
     }
 
-    /**
-     * restore redeemed back to user, only founder can do, if user made an error
-     */
-    function redeemRestore(address _to, uint256 tokenNum){
-        if( msg.sender != founder) throw;
-        if( redeem[_to] < tokenNum ) throw;
+    // get investors' locked amount of token
+    // this lockup will be released in 3 batches:
+    // 1. on delievery date
+    // 2. three months after the delivery date
+    // 3. six months after the delivery date
+    function getLockedAmount_investors(address _investor)
+        public
+		constant
+		returns (uint256)
+	{
+        uint256 delieveryDate = investors_deliveryDate[_investor];
+        uint256 lockedAmt = investors_locked[_investor];
 
-        redeem[_to] -= tokenNum;
-        balances[_to] += tokenNum;
+        if (now <= delieveryDate) {return lockedAmt;}
+        if (now <= delieveryDate + 90 days) {return lockedAmt.mul(2).div(3);}
+        if (now <= delieveryDate + 180 days) {return lockedAmt.mul(1).div(3);}
+
+        return 0;
     }
 
+    // set lockup for contributors
+    function setLockup_contributors(address _contributor, uint256 _value, uint256 _countdownDate)
+        public
+        onlyOwner
+    {
+        require(_contributor != address(0));
 
-    /**
-     * Emergency Stop ICO.
-     */
-    function halt() {
-        if (msg.sender!=founder) throw;
-        halted = true;
+        contributors_locked[_contributor] = _value;
+        contributors_countdownDate[_contributor] = _countdownDate;
+        UpdatedLockingState("contributor", _contributor, _value, _countdownDate);
     }
 
-    function unhalt() {
-        if (msg.sender!=founder) throw;
-        halted = false;
+    // set lockup for strategic investor
+    function setLockup_investors(address _investor, uint256 _value, uint256 _delieveryDate)
+        public
+        onlyOwner
+    {
+        require(_investor != address(0));
+
+        investors_locked[_investor] = _value;
+        investors_deliveryDate[_investor] = _delieveryDate;
+        UpdatedLockingState("investor", _investor, _value, _delieveryDate);
     }
 
-    // only owner can kill
-    function kill() { 
-        if (msg.sender == owner) suicide(owner); 
+	// Transfer amount of tokens from sender account to recipient.
+    function transfer(address _to, uint _value)
+        public
+        canTransfer(msg.sender, _value)
+		returns (bool success)
+	{
+        return super.transfer(_to, _value);
     }
 
-
-    /**
-     * Change founder address (where ICO ETH is being forwarded).
-     */
-    function changeFounder(address newFounder) {
-        if (msg.sender!=founder) throw;
-        founder = newFounder;
+	// Transfer amount of tokens from a specified address to a recipient.
+    function transferFrom(address _from, address _to, uint _value)
+        public
+        canTransfer(_from, _value)
+		returns (bool success)
+	{
+        return super.transferFrom(_from, _to, _value);
     }
-
-    /**
-     * ERC 20 Standard Token interface transfer function
-     */
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        if (block.number <= (endBlock + transferLockup) && msg.sender!=founder) throw;
-
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        if (balances[msg.sender] >= _value && (balances[_to] + _value) > balances[_to]) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
-
-    }
-
-    /**
-     * ERC 20 Standard Token interface transfer function
-     */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (block.number <= (endBlock + transferLockup) && msg.sender!=founder) throw;
-
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && (balances[_to] + _value) > balances[_to]) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    /**
-     * Do not allow direct deposits.
-     *
-     * All crowdsale depositors must have read the legal agreement.
-     * This is confirmed by having them signing the terms of service on the website.
-     * The give their crowdsale Ethereum source address on the website.
-     * donate() takes data as input and rejects all deposits that do not have
-     * signature you receive after reading terms of service.
-     *
-     */
-    function() {
-        throw;
-    }
-
 }
